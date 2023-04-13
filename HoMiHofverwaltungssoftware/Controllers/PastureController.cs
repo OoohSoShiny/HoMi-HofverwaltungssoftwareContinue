7ac77015cc2bedca579d102dbf7252805603ff2a
Shiny <40174971+OoohSoShiny@users.noreply.github.com>
2023-04-13 14:18:11 +0200

    PastureController api url improved

--- HoMiHofverwaltungssoftware/Controllers/PastureController.cs
+++ HoMiHofverwaltungssoftware/Controllers/PastureController.cs
@@ -51,7 +51,6 @@ namespace HoMiHofverwaltungssoftware.Controllers
         }
 
         // PUT: api/Pastures/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPastureModel(int id, PastureModel pastureModel)
         {
